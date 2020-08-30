36f5f4e51974b9e1f81d12e2ea2e26c920b33829
Kristofer Engelbrektsson <engelbrk@gmail.com>
2020-08-30 14:55:13 +0200

    implemented search venue route

--- app.py
+++ app.py
@@ -134,13 +134,17 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  search_term=request.form.get('search_term', '')
+  venues = Venue.query.filter(Venue.name.ilike("%" + search_term + "%")).all()
+  print(venues)
+  
+  data = []
+  for venue in venues:
+    data.append(venue)
+  
   response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(venues),
+    "data": data
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
