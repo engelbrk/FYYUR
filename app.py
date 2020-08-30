778122e4a0c8bffe2d5916af5dd04402c4f41a17
Kristofer Engelbrektsson <engelbrk@gmail.com>
2020-08-30 14:59:44 +0200

    implemented artist search route

--- app.py
+++ app.py
@@ -131,12 +131,9 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  
   search_term=request.form.get('search_term', '')
   venues = Venue.query.filter(Venue.name.ilike("%" + search_term + "%")).all()
-  print(venues)
   
   data = []
   for venue in venues:
@@ -272,17 +269,18 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+  search_term=request.form.get('search_term', '')
+  artists = Artist.query.filter(Artist.name.ilike("%" + search_term + "%")).all()
+  
+  data = []
+  for artist in artists:
+    data.append(artist)
+  
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(artists),
+    "data": data
   }
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
